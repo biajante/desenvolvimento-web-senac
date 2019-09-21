c4789d6042fdbf9e141b0902f9a5d1c00520ca3a
Aluno <Aluno@PC->
2019-09-20 21:35:00 -0300

    Aula 20-09-2019

--- src/main/java/com/github/braully/dws/AplicacaoWeb.java
+++ src/main/java/com/github/braully/dws/AplicacaoWeb.java
@@ -33,29 +33,54 @@ import org.springframework.web.context.ServletContextAware;
 @EnableWebSecurity
 @SpringBootApplication
 public class AplicacaoWeb extends WebSecurityConfigurerAdapter implements ServletContextAware {
-
+    
     public static void main(String... args) {
         SpringApplication.run(AplicacaoWeb.class, args);
     }
-
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        //return new BCryptPasswordEncoder();
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence cs) {
+                return cs.toString();
+            }
+            
+            @Override
+            public boolean matches(CharSequence cs, String string) {
+                return cs.toString().equals(string);
+            }
+        };
+        
     }
-
+    
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("usuario")
                 .password(passwordEncoder().encode("123"))
                 .roles("estagiario").build();
-
+        
         UserDetails admin = User.withUsername("sky")
                 .password(passwordEncoder().encode("net"))
                 .roles("fodão", "fodãoSPlus").build();
-
+        
         return new InMemoryUserDetailsManager(user, admin);
     }
-
+    
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(datasource())
+                .usersByUsernameQuery(
+                        "select login as username, senha as password, true as enabled from usuario where login=?")
+                .authoritiesByUsernameQuery(
+                        "select login as username, g.grupo as role "
+                        + "from grupo g"
+                        + " inner join usuario_grupos_usuario ug on ug.grupos_usuario_id = g.id"
+                        + " inner join usuario u on ug.usuario_id = u.id "
+                        + "where login=?");
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -71,7 +96,7 @@ public class AplicacaoWeb extends WebSecurityConfigurerAdapter implements Servle
                 .permitAll().and()
                 .logout().permitAll();
     }
-
+    
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
         FacesServlet servlet = new FacesServlet();
@@ -79,33 +104,33 @@ public class AplicacaoWeb extends WebSecurityConfigurerAdapter implements Servle
 //        ServletRegistrationBean reg = new ServletRegistrationBean(servlet, "*.senac");
 //        return reg;
     }
-
+    
     @Bean
     public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
         return new ServletListenerRegistrationBean<ConfigureListener>(
                 new ConfigureListener());
     }
-
+    
     @Override
     public void setServletContext(ServletContext servletContext) {
         servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
         //servletContext.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".html");
     }
-
+    
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource datasource() {
         return DataSourceBuilder.create().build();
     }
-
+    
     @Bean
     public static CustomScopeConfigurer viewScope() {
         CustomScopeConfigurer configurer = new CustomScopeConfigurer();
-
+        
         configurer.setScopes(
                 Map.of("view", new ViewScope())
         );
-
+        
         return configurer;
     }
 }
